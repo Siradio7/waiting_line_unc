8b9ada26109c09c35aa3923190c1f8b68fde0ba3
Mamadou Siradiou Diallo <mamadousiradioudiallo4@gmail.com>
2024-06-09 22:40:12 +0000

    Added student model

---
+++ src/main/java/com/kais/solutions/waiting_line/model/Student.java
@@ -0,0 +1,29 @@
+package com.kais.solutions.waiting_line.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private String prenom;
+    private String matricule;
+    private boolean scolaritePayee;
+
+    // Getters et setters
+}
+
