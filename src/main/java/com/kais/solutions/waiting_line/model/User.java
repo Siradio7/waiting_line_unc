2d6289758281633735ae001a1cb66325aacb1fda
Mamadou Siradiou Diallo <mamadousiradioudiallo4@gmail.com>
2024-06-09 22:46:01 +0000

    Added user model

---
+++ src/main/java/com/kais/solutions/waiting_line/model/User.java
@@ -0,0 +1,35 @@
+package com.kais.solutions.waiting_line.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private boolean enabled;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+
+    // Getters et setters
+}
