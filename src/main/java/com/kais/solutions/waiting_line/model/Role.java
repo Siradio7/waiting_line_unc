f1487244acf1f63cc4469f24a362cce64ef3409a
Mamadou Siradiou Diallo <mamadousiradioudiallo4@gmail.com>
2024-06-09 22:46:13 +0000

    Added role model

---
+++ src/main/java/com/kais/solutions/waiting_line/model/Role.java
@@ -0,0 +1,28 @@
+package com.kais.solutions.waiting_line.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    // Getters et setters
+}
