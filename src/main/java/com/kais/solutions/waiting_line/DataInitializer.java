b1dce584cf423dc7da85a9a9ba331f3b12d46b5c
Mamadou Siradiou Diallo <mamadousiradioudiallo4@gmail.com>
2024-06-09 22:50:47 +0000

    Initializing one user identified by "admin" in the database

--- src/main/java/com/kais/solutions/waiting_line/DataInitializer.java
+++ src/main/java/com/kais/solutions/waiting_line/DataInitializer.java
@@ -1,16 +1,33 @@
 package com.kais.solutions.waiting_line;
 
 import com.kais.solutions.waiting_line.model.Student;
+import com.kais.solutions.waiting_line.model.Role;
+import com.kais.solutions.waiting_line.model.User;
 import com.kais.solutions.waiting_line.repository.StudentRepository;
+import com.kais.solutions.waiting_line.repository.RoleRepository;
+import com.kais.solutions.waiting_line.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.List;
 
 @Component
 public class DataInitializer implements CommandLineRunner {
 
     @Autowired
     private StudentRepository StudentRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public void run(String... args) throws Exception {
@@ -28,6 +45,19 @@ public class DataInitializer implements CommandLineRunner {
 
         StudentRepository.save(Student1);
         StudentRepository.save(Student2);
+
+        // Initialiser un utilisateur admin
+        Role adminRole = new Role();
+        adminRole.setName("ADMIN");
+        roleRepository.save(adminRole);
+
+        User admin = new User();
+        admin.setUsername("admin");
+        admin.setPassword(passwordEncoder.encode("admin"));
+        admin.setEnabled(true);
+        admin.setRoles(new HashSet<>(List.of(adminRole)));
+
+        userRepository.save(admin);
     }
 }
 
